38fb4b7f8cb6cdbf27001a730782c8dbbf19446a
ruslanv-r <rvregi@gmail.com>
2022-02-21 20:57:29 +0900

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+
+
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class Calculator {
+
+
+    private Supplier supplier;
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+
+
+}
